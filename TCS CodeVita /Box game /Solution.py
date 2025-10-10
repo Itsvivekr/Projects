8ff654d2149a4d3bb0112ed3e7f3a3e1aa6ac695
Vivek R <164515008+Itsvivekr@users.noreply.github.com>
2025-10-10 20:30:09 +0530

    Update Solution.py

--- TCS CodeVita /Box game /Solution.py
+++ TCS CodeVita /Box game /Solution.py
@@ -14,34 +14,52 @@ def is_solved(face):
     color = face[0][0]
     return all(cell == color for row in face for cell in row)
 
-def rotate_face(face, direction):
-    N = len(face)
-    new_face = [[None]*N for _ in range(N)]
+def rotate_matrix(mat, direction):
+    N = len(mat)
+    new_mat = [[None]*N for _ in range(N)]
     if direction == 'left':
         for i in range(N):
             for j in range(N):
-                new_face[N-j-1][i] = face[i][j]
+                new_mat[N-j-1][i] = mat[i][j]
     elif direction == 'right':
         for i in range(N):
             for j in range(N):
-                new_face[j][N-i-1] = face[i][j]
-    return new_face
+                new_mat[j][N-i-1] = mat[i][j]
+    return new_mat
 
-def apply_instruction(cube, instr):
-    cube = copy.deepcopy(cube)
+def deep_copy_cube(cube):
+    return {face: [row[:] for row in cube[face]] for face in cube}
+
+def apply_instruction(cube, instr, N):
+    cube = deep_copy_cube(cube)
     parts = instr.split()
     if parts[0] == 'turn':
         if parts[1] == 'left':
             cube['front'], cube['left'], cube['back'], cube['right'] = cube['right'], cube['front'], cube['left'], cube['back']
-            cube['top'] = rotate_face(cube['top'], 'right')
-            cube['base'] = rotate_face(cube['base'], 'left')
+            cube['top'] = rotate_matrix(cube['top'], 'right')
+            cube['base'] = rotate_matrix(cube['base'], 'left')
         elif parts[1] == 'right':
             cube['front'], cube['right'], cube['back'], cube['left'] = cube['left'], cube['front'], cube['right'], cube['back']
-            cube['top'] = rotate_face(cube['top'], 'left')
-            cube['base'] = rotate_face(cube['base'], 'right')
+            cube['top'] = rotate_matrix(cube['top'], 'left')
+            cube['base'] = rotate_matrix(cube['base'], 'right')
     elif parts[0] == 'rotate':
-        # Simplified: no-op for now
-        pass
+        side = parts[1]
+        if side == 'front':
+            cube['front'], cube['base'], cube['back'], cube['top'] = cube['top'], cube['front'], cube['base'], cube['back']
+            cube['left'] = rotate_matrix(cube['left'], 'right')
+            cube['right'] = rotate_matrix(cube['right'], 'left')
+        elif side == 'back':
+            cube['front'], cube['top'], cube['back'], cube['base'] = cube['base'], cube['front'], cube['top'], cube['back']
+            cube['left'] = rotate_matrix(cube['left'], 'left')
+            cube['right'] = rotate_matrix(cube['right'], 'right')
+        elif side == 'left':
+            cube['top'], cube['left'], cube['base'], cube['right'] = cube['right'], cube['top'], cube['left'], cube['base']
+            cube['front'] = rotate_matrix(cube['front'], 'left')
+            cube['back'] = rotate_matrix(cube['back'], 'right')
+        elif side == 'right':
+            cube['top'], cube['right'], cube['base'], cube['left'] = cube['left'], cube['top'], cube['right'], cube['base']
+            cube['front'] = rotate_matrix(cube['front'], 'right')
+            cube['back'] = rotate_matrix(cube['back'], 'left')
     else:
         side, idx, direction = parts
         idx = int(idx) - 1
@@ -54,12 +72,12 @@ def apply_instruction(cube, instr):
                 row = row[-1:] + row[:-1]
             face[idx] = row
         elif direction in ['up', 'down']:
-            col = [face[i][idx] for i in range(len(face))]
+            col = [face[i][idx] for i in range(N)]
             if direction == 'up':
                 col = col[1:] + col[:1]
             else:
                 col = col[-1:] + col[:-1]
-            for i in range(len(face)):
+            for i in range(N):
                 face[i][idx] = col[i]
         cube[side] = face
     return cube
@@ -67,11 +85,11 @@ def apply_instruction(cube, instr):
 def solve():
     N, K, cube, instructions = read_input()
     for i in range(K):
-        temp_cube = copy.deepcopy(cube)
+        temp_cube = deep_copy_cube(cube)
         for j in range(K):
             if i == j:
                 continue
-            temp_cube = apply_instruction(temp_cube, instructions[j])
+            temp_cube = apply_instruction(temp_cube, instructions[j], N)
         for face in temp_cube.values():
             if is_solved(face):
                 print(instructions[i])
@@ -87,11 +105,11 @@ def solve():
                         continue
                     cube[f1][i][j] = c
                     for i2 in range(K):
-                        temp_cube = copy.deepcopy(cube)
+                        temp_cube = deep_copy_cube(cube)
                         for j2 in range(K):
                             if i2 == j2:
                                 continue
-                            temp_cube = apply_instruction(temp_cube, instructions[j2])
+                            temp_cube = apply_instruction(temp_cube, instructions[j2], N)
                         for face in temp_cube.values():
                             if is_solved(face):
                                 print("Faulty")
